65bca8a80e2c224ae273c493cf0550e6d93017c7
ikiris <4852950+ikiris@users.noreply.github.com>
2023-08-13 03:49:30 -0500

    dev checkin

--- prod/prodserver/server.go
+++ prod/prodserver/server.go
@@ -113,6 +113,7 @@ func loadKeyPair() (credentials.TransportCredentials, error) {
 	return credentials.NewTLS(tlsConfig), nil
 }
 
+// auth stuff goes here. Mostly so the healthchecks on mux can work, also any user auth/z for service.
 func middlefunc(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
 	// get client tls info
 	if p, ok := peer.FromContext(ctx); ok {
